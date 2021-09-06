bf05d2cdc89d8fb8fc8655e51221ce57b98cdb1d
Christian M. Lux <chris@Christians-MacBook-Pro.local>
2021-09-06 17:37:10 +0200

    feat: starts implementing navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
+  <NavBar />
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
-  <router-view />
+  <main>
+    <router-view />
+  </main>
 </template>
+
+<script>
+import NavBar from "@/components/NavBar/NavBar.vue";
+
+export default {
+  name: "App",
+  components: { NavBar },
+};
+</script>
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Open+Sans&display=swap");
@@ -22,13 +34,18 @@
   line-height: 1.5;
   font-family: "Open Sans", sans-serif;
 }
+body {
+  margin: 0 auto;
+}
 #app {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: var(--dark);
   background-color: var(--bright);
 }
-
+main {
+  padding-left: 5rem;
+}
 #nav {
   padding: 30px;
 
---
+++ src/components/NavBar/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav class="navbar">
+    <ul></ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+};
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  width: 5rem;
+  height: 100vh;
+  position: fixed;
+  background-color: var(--dark);
+}
+</style>
