5215600ac1c814da168f3374312d8c0096c34587
CML <christian.m.lux@gmail.com>
2021-09-06 19:07:07 +0200

    feat: adds routes & icons

--- src/components/NavBar/NavBar.vue
+++ src/components/NavBar/NavBar.vue
@@ -1,12 +1,49 @@
 <template>
   <nav class="navbar">
-    <ul></ul>
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <router-link :to="homeRoute" class="nav-link">
+          <component :is="routeIconHome"></component>
+          <span class="link-text">Home</span>
+        </router-link>
+      </li>
+      <li class="nav-item">
+        <router-link :to="profileRoute" class="nav-link">
+          <component :is="routeIconProfile"></component>
+          <span class="link-text"></span>
+        </router-link>
+      </li>
+      <li class="nav-item">
+        <router-link :to="storyRoute" class="nav-link">
+          <component :is="routeIconStory"></component>
+          <span class="link-text"></span>
+        </router-link>
+      </li>
+      <li class="nav-item">
+        <router-link :to="contactRoute" class="nav-link">
+          <component :is="routeIconContact"></component>
+          <span class="link-text"></span>
+        </router-link>
+      </li>
+    </ul>
   </nav>
 </template>
 
 <script>
 export default {
   name: "NavBar",
+  data() {
+    return {
+      routeIconHome: "md-home-icon",
+      homeRoute: "/",
+      routeIconProfile: "md-person-icon",
+      profileRoute: "/profile",
+      routeIconStory: "md-book-icon",
+      storyRoute: "/story",
+      routeIconContact: "md-mail-icon",
+      contactRoute: "/contact",
+    };
+  },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -3,5 +3,8 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import AllIosIcon from "vue-ionicons/dist/ionicons.js";
 
-createApp(App).use(store).use(router).mount("#app");
+const app = createApp(App).use(store).use(router);
+app.use(AllIosIcon);
+app.mount("#app");
