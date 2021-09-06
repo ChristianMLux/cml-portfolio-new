c4dd43e5f0a7783b66f813c6682e9970d8953c5a
ChristianMLux <christian.m.lux@gmail.com>
2021-09-06 14:13:03 +0200

    feat: implements basic color variables and font family/size

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,26 @@
 </template>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Open+Sans&display=swap");
+:root {
+  --primary-color: #da6e01;
+  --primary-color-light: #fd9935;
+  --success: #a3db33;
+  --error: #ed3221;
+  --warning: #ffc642;
+  --bright: #f2f2f3;
+  --grey: #c8cdd0;
+  --dark-grey: #415058;
+  --dark: #1d272a;
+  font-size: 18px;
+  line-height: 1.5;
+  font-family: "Open Sans", sans-serif;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: var(--dark);
+  background-color: var(--bright);
 }
 
 #nav {
