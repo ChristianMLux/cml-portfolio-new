3f0807fff75dbd32e2f21de861e93b8583d74df7
CML <christian.m.lux@gmail.com>
2021-09-23 16:47:12 +0200

    feat: starts implementing CV Component

---
+++ src/components/CVList/CVListElement.vue
@@ -0,0 +1,57 @@
+<template>
+  <li>
+    <h2>{{ expHeading }}</h2>
+    <p>{{ expSubheading }}</p>
+    <ul>
+      <li>
+        <p>Hardskills:</p>
+        <p>{{ expHardSkills }}</p>
+      </li>
+      <li>
+        <p>Tooling:</p>
+        <p>{{ expToolings }}</p>
+      </li>
+      <li>
+        <p>Prinzipien:</p>
+        <p>{{ expPrinciples }}</p>
+      </li>
+      <li>
+        <p>Softskills:</p>
+        <p>{{ expSoftSkills }}</p>
+      </li>
+      <li>
+        <p>Projekte:</p>
+        <p>{{ expProjects }}</p>
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "CVListElement",
+  props: {
+    expHeading: {
+      type: String,
+    },
+    expSubheading: {
+      type: String,
+    },
+    expHardSkills: {
+      type: String,
+    },
+    expToolings: {
+      type: String,
+    },
+    expPrinciples: {
+      type: String,
+    },
+    expSoftSkills: {
+      type: String,
+    },
+    expProjects: {
+      type: String,
+    },
+  },
+};
+</script>
---
+++ src/views/CV.vue
@@ -0,0 +1,10 @@
+<template>
+  <CVMain />
+</template>
+<script>
+import CVMain from "@/components/CVMain/CVMain.vue";
+export default {
+  name: "CV",
+  components: { CVMain },
+};
+</script>
