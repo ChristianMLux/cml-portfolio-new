e54161787a8d7db2b0b7abaa294201c704d4985b
CML <christian.m.lux@gmail.com>
2021-09-26 15:22:06 +0200

    feat: implements profile component with content

---
+++ src/components/MyProfile/MyProfile.vue
@@ -0,0 +1,59 @@
+<template>
+  <section>
+    <h2>Me in a nutshell</h2>
+    <h3>Zu meiner Person</h3>
+    <ul class="pers-info-list">
+      <li>
+        <p>Geburtsdatum:</p>
+        <p>01.11.1990</p>
+      </li>
+      <li>
+        <p>Wohnort:</p>
+        <p>Stuttgart</p>
+      </li>
+      <li>
+        <p>Familienstand:</p>
+        <p>Ledig</p>
+      </li>
+    </ul>
+    <h3>Zu meinen Fähigkeiten</h3>
+    <ul class="skill-info-list">
+      <li>
+        <p>Hardskills:</p>
+        <p>
+          HTML, CSS, JavaScript, Java, SQL, jQuery, PHP, Python, vb.NET,
+          Marketing, BWL
+        </p>
+      </li>
+      <li>
+        <p>Toolings:</p>
+        <p>
+          VSCode, Git(-Hub), Vue, Cypress, Firebase, NPM, Xampp, NetBeans, Adobe
+          Suite, jMonkey, NodeJS, Eclipse, ERP
+        </p>
+      </li>
+      <li>
+        <p>Prinzipien:</p>
+        <p>
+          Versionierung, GitFlow, CleanCode, REST, OOP, wissenschaftliches
+          Arbeiten, ER-Diagramme, Designregeln, Readability, Don't repeat
+          yourself, Marktmechanismen, Nachhaltigkeit, Marketingmix
+        </p>
+      </li>
+      <li>
+        <p>Softskills:</p>
+        <p>
+          SCRUM, Teamwork, Präsentationen, sicheres Auftreten, Geduld,
+          realistische Erwartungshaltung, Zeitmanagement, flexibel,
+          eigenständig, adaptiv, zielstrebig
+        </p>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "MyProfile",
+};
+</script>
