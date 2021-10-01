f5cd9912592e6e23a2c9ed040df6b6f1e9d68fdc
CML <christian.m.lux@gmail.com>
2021-10-01 17:44:36 +0200

    feat: adds story part

---
+++ src/components/MyStory/MainStory.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="ms__story-section">
+    <h2 class="story__heading">
+      Meine Geschichte...(tl:dr=<router-link to="/profile">Profile</router-link
+      >)
+    </h2>
+    <p class="story__text">
+      Meine Geschichte mit Layouts und Code startete anfang 2003, als ich
+      angefangen habe durch verschiedene Foren zu stöbern. In diesen Foren war
+      es üblich eine persönliche Signatur einzustellen, die dann unter den
+      Foreneinträgen angezeigt wurde. Um für mich ein Bild für diese Signatur zu
+      basteln, hatte ich den ersten Kontakt mit Fireworks MX, was danach zu
+      Adobes Fireworks CS3 wurde.
+    </p>
+    <div class="img-wrapper">
+      <img src="@/assets/showroom/signaturen.png" />
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: "MainStory",
+};
+</script>
+
+<style lang="scss" scoped>
+.story__text {
+  max-width: 70vw;
+  padding: 1rem 0 0 1rem;
+  margin-right: 12rem;
+  line-height: 1.5rem;
+  text-align: justify;
+  font-size: 1.1em;
+}
+.img-wrapper {
+  max-width: 70vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  img {
+    max-width: 100%;
+  }
+}
+</style>
---
+++ src/views/Story.vue
@@ -0,0 +1,11 @@
+<template>
+  <MainStory />
+</template>
+
+<script>
+import MainStory from "@/components/MyStory/MainStory.vue";
+export default {
+  name: "Story",
+  components: { MainStory },
+};
+</script>
